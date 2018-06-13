d5b57c7744510d53ae5ba08b18be0a3fc2a61ec6
silva.gabriels@hotmail.com <silva.gabriels@hotmail.com>
2018-06-12 23:03:13 -0300

    Medias octave e DET

---
+++ Lab03/plotSeria2l.m
@@ -0,0 +1,60 @@
+clear all;
+close all;
+
+pkg load instrument-control;
+
+s = serial('COM6');
+
+set(s, 'baudrate', 57600);     % See List Below
+set(s, 'bytesize', 8);        % 5, 6, 7 or 8
+set(s, 'parity', 'n');        % 'n' or 'y'
+set(s, 'stopbits', 1);        % 1 or 2
+set(s, 'timeout', 123);     % 12.3 Seconds as an example here
+
+%set(s,'InputBufferSize',100);
+%set(s,'flowcontrol','hardware');
+%set(s,'baudrate',57600);
+%set(s,'parity','none');
+%set(s,'DataBits',8);
+%set(s,'stopbit',1);
+%set(s,'timeout',10);
+%disp(get(s,'name'));
+%prop(1)-(get(s,'baudrate'));
+%prop(2)-(get(s,'DataBits'));
+%prop(3)-(get(s,'StopBit'));
+%prop(4)-(get(s,'InputBufferSize'));
+%disp(['Port Setup Done!!',num2str(prob)]);
+fopen(s);
+t=1;
+disp('Running');
+x=0;
+while(t<2000)
+    serialMed = 0;
+    
+    for i = 1:8
+      a = srl_read(s,1);
+      a= max(a);
+      serialMed = serialMed + a;
+    endfor
+    
+    serialMed = serialMed/8;
+    x = [x serialMed];
+    
+    plot(x);
+    axis auto;
+    grid on;
+    title('Leitura Serial');
+    xlabel('Tempo em (S)')
+    ylabel('Qtd de Luz')
+    h.XDataSource = 'x';
+    h.YDataSource = 'y';
+    grid on;
+    ylim([0 300])
+    disp([num2str(t),'th iteration max= ',num2str(a)]);
+    hold on;
+    t = t+1;
+    a = 0;
+    drawnow;
+end
+fclose(s);
+
